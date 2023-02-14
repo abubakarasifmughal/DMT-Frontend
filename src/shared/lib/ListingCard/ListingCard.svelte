faee9bcb6a643ab89645dd97f05619cd482c5d50
abubakar2000 <121317969+mo2ooo@users.noreply.github.com>
2023-02-14 14:54:50 +0500

    cards

--- src/shared/lib/ListingCard/ListingCard.svelte
+++ src/shared/lib/ListingCard/ListingCard.svelte
@@ -4,25 +4,27 @@
   export let listing;
 </script>
 
-<div class="card shadow mb-3" style="overflow: hidden;">
-  {#if listing.listingImages.length > 0}
-    <img
-      src={listing.listingImages[0].address}
-      alt=""
-      style="object-fit: cover;object-position: center;"
-      height="350px"
-      width="100%"
-    />
-  {/if}
-  <div class="p-4">
-    <div class="fw-bold" style="font-size: x-large;">{listing.headline}</div>
-    <div class="text-secondary">{listing.address}</div>
-    <div class="text-secondary" style="font-size: 15px;line-height:19px">
-      {listing.description}
+<Link to="/listing/{listing.id}" class="text-decoration-none text-dark">
+  <div class="card shadow mb-3" style="overflow: hidden;">
+    {#if listing.listingImages.length > 0}
+      <img
+        src={listing.listingImages[0].address}
+        alt=""
+        style="object-fit: cover;object-position: center;"
+        height="350px"
+        width="100%"
+      />
+    {/if}
+    <div class="p-4">
+      <div class="fw-bold" style="font-size: x-large;color:#9427F7">
+        {listing.headline}
+      </div>
+      <div class="text-secondary">{listing.address}</div>
+      <div class="text-secondary" style="font-size: 15px;line-height:19px">
+        {listing.description}
+      </div>
+      <!-- <br /> -->
+      <!-- <div class="btn btn-light border">Check In</div> -->
     </div>
-    <br />
-    <Link to="/listing/{listing.id}">
-      <div class="btn btn-light border">Check In</div>
-    </Link>
   </div>
-</div>
+</Link>
