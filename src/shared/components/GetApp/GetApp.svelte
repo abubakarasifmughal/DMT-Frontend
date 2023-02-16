f6a20a0bbdd68397ebf5a8bfda60e25a68afa72c
Waleed <waleedmukhtar1999@gmail.com>
2023-02-17 03:00:04 +0500

    GET THE APP DONE

---
+++ src/shared/components/GetApp/GetApp.svelte
@@ -0,0 +1,106 @@
+<script>
+</script>
+
+<div class="container-fluid">
+  <div class="row mx-md-5">
+    <div class="col-md-6 col-sm-12 py-4 px-4 getAppBackground">
+      <h3>
+        Elevate your experience, Download our app now & manage your trips with a
+        tap!
+      </h3>
+      <p>
+        Save 10% on tours and activities is as easy as booking with the DMT App.
+      </p>
+
+      
+        <div class="flex">
+          <div >
+            <button>
+              <img
+                src="/assets/static/images/index/store-googleplay.png"
+                alt="qr"
+                class="btns"
+              />
+            </button>
+            <button>
+              <img
+                src="/assets/static/images/index/store-app.png"
+                alt="qr"
+                class="btns"
+              />
+            </button>
+          </div>
+          
+            <img
+              src="/assets/static/images/index/qr-code.png"
+              alt="qr"
+              class="qr"
+            />
+          
+        
+
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12 airbnb">
+      <img
+        src="/assets/static/images/index/Screen.png"
+        alt="screen"
+        class="screen"
+      />
+    </div>
+  </div>
+</div>
+
+<style>
+  .getAppBackground {
+    background-color: #f5eef6;
+  }
+  h3 {
+    font-family: "Poppins";
+    font-size: 22px;
+    font-weight: 1000;
+    line-height: 33px;
+    letter-spacing: 0px;
+    text-align: left;
+    width: 360px;
+  }
+  p {
+    width: 340px;
+    font-size: 14px;
+    margin-top: 20px;
+  }
+  .flex{
+
+    display: flex;
+    margin-top: 25px;
+  }
+  button {
+    border: none;
+    display: block;
+    margin-right: 20px;
+    
+  }
+
+  .airbnb {
+    background-image: url("/assets/static/images/index/airbnb.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .qr {
+    width: 130px;
+    height:60%;
+  }
+
+  .screen {
+    margin-top: 20px;
+    margin-bottom: -20px;
+    /* z-index: 20; */
+    width: 200px;
+    /* position: absolute; */
+  }
+  .btns {
+    width: 140px;
+    margin-bottom: 25px;
+  }
+</style>
