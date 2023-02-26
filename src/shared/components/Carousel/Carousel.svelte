07918337cfac3b34c48416d9e087e43baeca7604
egdose <57728907+egdose@users.noreply.github.com>
2023-02-27 00:49:06 +0500

    Update Carousel.svelte

--- src/shared/components/Carousel/Carousel.svelte
+++ src/shared/components/Carousel/Carousel.svelte
@@ -57,7 +57,7 @@
           class="card-slide d-flex text-center p-3 align-items-center justify-content-end flex-column rounded m-md-0 m-3"
           style="background-image: url({cards[currentCardIndex].image});"
         >
-          <div class="bg-white w-100 rounded p-3 text-start">
+          <!-- <div class="bg-white w-100 rounded p-3 text-start">
             <div>
               <div class="d-flex flex-row align-items-top">
                 <div
@@ -92,6 +92,46 @@
               </div>
               <div class="text-secondary fw-bold mt-1">
                 {cards[currentCardIndex].category}
+              </div>
+            </div>
+          </div> -->
+          <div class="bg-white w-100 rounded p-3 text-start">
+            <div>
+              <div>
+                <span class="text-secondary1 justify-content-between"
+                  ><div>
+                    <!-- <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="img"
+                      width="100%"
+                      height="100%"
+                      preserveAspectRatio="xMidYMid meet"
+                      viewBox="0 0 24 24"
+                      ><path
+                        fill="#9c59df"
+                        stroke="#9c59df"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M11.074 2.633c.32-.844 1.531-.844 1.852 0l2.07 5.734a.99.99 0 0 0 .926.633h5.087c.94 0 1.35 1.17.611 1.743L18 14a.968.968 0 0 0-.322 1.092L19 20.695c.322.9-.72 1.673-1.508 1.119l-4.917-3.12a1 1 0 0 0-1.15 0l-4.917 3.12c-.787.554-1.83-.22-1.508-1.119l1.322-5.603A.968.968 0 0 0 6 14l-3.62-3.257C1.64 10.17 2.052 9 2.99 9h5.087a.989.989 0 0 0 .926-.633l2.07-5.734Z"
+                      /></svg
+                    > -->
+                    <b>{cards[currentCardIndex].text}</b>
+                  </div>
+                  <div class="rat">
+                    <div><b>{cards[currentCardIndex].rating}</b></div>
+                    <div><b>({cards[currentCardIndex].review})</b></div>
+                  </div></span
+                >
+              </div>
+              <div />
+              <div class="label">
+                <div class="text-black">{cards[currentCardIndex].label}</div>
+              </div>
+              <div class="text-secondary">
+                <i class="bi bi-geo-alt-fill" />
+                {cards[currentCardIndex].address}
               </div>
             </div>
           </div>
